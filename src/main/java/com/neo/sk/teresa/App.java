2a257da5c32185eaef612ce857db6bacf04b456f
zhaorui <zhaorui@neotel.com.cn>
2016-07-01 15:59:00 +0800

    init

---
+++ src/main/java/com/neo/sk/teresa/App.java
@@ -0,0 +1,13 @@
+package com.neo.sk.teresa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
